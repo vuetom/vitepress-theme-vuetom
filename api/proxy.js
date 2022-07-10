c6c9bceb186d44ea898a9378dea1c1120310756a
Lauset <47267800+lauset@users.noreply.github.com>
2022-07-11 00:55:12 +0800

    chore: vercel proxy

---
+++ api/proxy.js
@@ -0,0 +1,15 @@
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+module.exports = (req, res) => {
+  let target = ''
+  if (req.url.startsWith('/vt')) {
+    target = 'https://vitepress-theme-vuetom.vercel.app'
+  }
+  createProxyMiddleware({
+    target,
+    changeOrigin: true,
+    pathRewrite: {
+      '^/vt/': '/',
+    },
+  })(req, res)
+}
