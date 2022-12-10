e3e38659ea06f59dfff032824e37f7c792f6974c
lauset <liujie_783@163.com>
2022-12-11 03:09:34 +0800

    pages 2022-12-11

---
+++ assets/guide_prodir.md.2ad3b822.js
@@ -0,0 +1,36 @@
+import{_ as s,o as n,c as a,e as p}from"./app.ca17caa8.js";const u=JSON.parse('{"title":"\u6846\u67B6\u76EE\u5F55","description":"Vuetom \u4E3B\u9898\u76EE\u5F55\u7ED3\u6784","frontmatter":{"title":"\u6846\u67B6\u76EE\u5F55","head":[["meta",{"name":"description","content":"Vuetom \u4E3B\u9898\u76EE\u5F55\u7ED3\u6784"}],["meta",{"name":"keywords","content":"project dir."}]]},"headers":[],"relativePath":"guide/prodir.md","lastUpdated":null}'),l={name:"guide/prodir.md"},e=p(`<h1 id="\u4E3B\u9898\u76EE\u5F55" tabindex="-1">\u4E3B\u9898\u76EE\u5F55 <a class="header-anchor" href="#\u4E3B\u9898\u76EE\u5F55" aria-hidden="true">#</a></h1><p>\u5728\u4F7F\u7528\u4E00\u4E2A\u6846\u67B6\uFF0C\u5176\u5B9E\u4E5F\u8981\u7B80\u5355\u4E86\u89E3\u4E0B\u8BE5\u6846\u67B6\u7684\u9879\u76EE\u6587\u4EF6\u7ED3\u6784\uFF0C\u8BF7\u5411\u4E0B\u770B\u5427\u3002</p><div class="language-bash line-numbers-mode"><button title="Copy Code" class="copy"></button><span class="lang">bash</span><pre class="shiki"><code><span class="line"><span style="color:#A6ACCD;">vuetom</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u251C\u2500 blog              \u535A\u5BA2\u4E3B\u9898Vue\u7EC4\u4EF6      \u6587\u4EF6\u5939</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u251C\u2500 doc               \u65B0\u7248\u6587\u6863\uFF0C\u9002\u914Dvp1.x  \u6587\u4EF6\u5939 </span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u251C\u2500 docs              \u65E7\u7248\u6587\u6863\u4E3B\u9898\u7EC4\u4EF6      \u6587\u4EF6\u5939</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u2502   \u251C\u2500 components        \u4E3B\u9898Vue\u7EC4\u4EF6      \u6587\u4EF6\u5939</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u2502   \u251C\u2500 composables       \u7EC4\u4EF6\u811A\u672C         \u6587\u4EF6\u5939</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u2502   \u251C\u2500 layouts           \u5E03\u5C40\u7EC4\u4EF6         \u6587\u4EF6\u5939</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u2502   \u2514\u2500 index.ts          \u4E3B\u9898\u5165\u53E3         \u6587\u4EF6 </span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u251C\u2500 icons             \u5171\u7528\u56FE\u6807Vue\u7EC4\u4EF6       \u6587\u4EF6\u5939</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u251C\u2500 styles            \u5168\u5C40SCSS\u6837\u5F0F         \u6587\u4EF6\u5939</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u251C\u2500 support           \u4F9B\u652F\u6301\u811A\u672C           \u6587\u4EF6\u5939</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u251C\u2500 types             \u89C4\u8303\u63CF\u8FF0             \u6587\u4EF6\u5939</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u251C\u2500 constant.ts       \u5E38\u91CF\u5B9A\u4E49             \u6587\u4EF6</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u2514\u2500 index.ts          \u4E3B\u9898\u5165\u53E3\u6587\u4EF6          \u6587\u4EF6</span></span>
+<span class="line"></span></code></pre><div class="line-numbers-wrapper"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br></div></div><p>\u63A5\u4E0B\u6765\u4ECB\u7ECD\u672C\u6587\u6863<strong>docs</strong>\u6587\u4EF6\u5939</p><div class="language-bash line-numbers-mode"><button title="Copy Code" class="copy"></button><span class="lang">bash</span><pre class="shiki"><code><span class="line"><span style="color:#A6ACCD;">docs</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u251C\u2500 .vitepress</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u2502   \u2514\u2500 config.ts   \u4E3B\u9898\u4E3B\u8981\u914D\u7F6E\u6587\u4EF6</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u2502 </span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u251C\u2500 public          \u9759\u6001\u8D44\u6E90\u6587\u4EF6</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u2502 </span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u251C\u2500 zh-CN           \u4E2D\u6587\u9875\u9762</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u2502    \u251C\u2500 feedback   \u7559\u8A00\u53CD\u9988</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u2502    \u251C\u2500 guide      \u6307\u5F15</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u2502    \u251C\u2500 mdshow     Markdown\u793A\u4F8B</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u2502    \u251C\u2500 menu       UI\u7EC4\u4EF6</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u2502    \u251C\u2500 styl       \u4E3B\u9898\u4E0E\u6837\u5F0F</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u2502    \u2514\u2500 index.md   \u4E2D\u6587\u9996\u9875</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u2502 </span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u251C\u2500 crowdin.yml      \u591A\u56FD\u8BED\u8A00\u914D\u7F6E</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u251C\u2500 index.md         \u9879\u76EE\u9996\u9875</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u251C\u2500 CHANGELOG.md     \u66F4\u65B0\u65E5\u5FD7</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u251C\u2500 package.json     \u5305\u914D\u7F6E</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u251C\u2500 README.md        \u9879\u76EE\u8BF4\u660E</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u251C\u2500 tsconfig.json    ts\u914D\u7F6E</span></span>
+<span class="line"><span style="color:#A6ACCD;">  \u2514\u2500 vite.config.ts   vite\u914D\u7F6E</span></span>
+<span class="line"></span></code></pre><div class="line-numbers-wrapper"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br><span class="line-number">17</span><br><span class="line-number">18</span><br><span class="line-number">19</span><br><span class="line-number">20</span><br><span class="line-number">21</span><br></div></div>`,5),c=[e];function r(o,i,t,b,C,A){return n(),a("div",null,c)}const d=s(l,[["render",r]]);export{u as __pageData,d as default};
---
+++ assets/mdshow_custom.md.1baad59a.js
@@ -0,0 +1,20 @@
+import{_ as a,o as e,c as l,a as s,b as p,t as c,e as t}from"./app.ca17caa8.js";const D=JSON.parse('{"title":"\u81EA\u5B9A\u4E49\u8BED\u6CD5","description":"\u81EA\u5B9A\u4E49 Markdown \u8BED\u6CD5","frontmatter":{"title":"\u81EA\u5B9A\u4E49\u8BED\u6CD5","head":[["meta",{"name":"description","content":"\u81EA\u5B9A\u4E49 Markdown \u8BED\u6CD5"}],["meta",{"name":"keywords","content":"markdown custom"}]]},"headers":[{"level":2,"title":"\u4FE1\u606F\u6846","slug":"\u4FE1\u606F\u6846","link":"#\u4FE1\u606F\u6846","children":[]}],"relativePath":"mdshow/custom.md","lastUpdated":null}'),r={name:"mdshow/custom.md"},i={id:"frontmatter-title",tabindex:"-1"},o=s("a",{class:"header-anchor",href:"#frontmatter-title","aria-hidden":"true"},"#",-1),m=t(`<h2 id="\u4FE1\u606F\u6846" tabindex="-1">\u4FE1\u606F\u6846 <a class="header-anchor" href="#\u4FE1\u606F\u6846" aria-hidden="true">#</a></h2><div class="language-md line-numbers-mode"><button title="Copy Code" class="copy"></button><span class="lang">md</span><pre class="shiki"><code><span class="line"><span style="color:#A6ACCD;">::: tip \u4F7F\u7528TIPS\u4EE3\u66FF</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u63D0\u793A\u4FE1\u606F</span></span>
+<span class="line"><span style="color:#A6ACCD;">:::</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A6ACCD;">::: info</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u4FE1\u606F\u6D88\u606F</span></span>
+<span class="line"><span style="color:#A6ACCD;">:::</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A6ACCD;">::: warning</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u8B66\u544A\u6D88\u606F</span></span>
+<span class="line"><span style="color:#A6ACCD;">:::</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A6ACCD;">::: danger</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u5371\u9669\u6D88\u606F</span></span>
+<span class="line"><span style="color:#A6ACCD;">:::</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A6ACCD;">::: details Details</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u8BE6\u7EC6\u4FE1\u606F</span></span>
+<span class="line"><span style="color:#A6ACCD;">:::</span></span>
+<span class="line"></span></code></pre><div class="line-numbers-wrapper"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br><span class="line-number">17</span><br><span class="line-number">18</span><br><span class="line-number">19</span><br></div></div><p>\u6548\u679C\u5982\u4E0B\uFF1A</p><div class="tip custom-block"><p class="custom-block-title">\u4F7F\u7528TIPS\u4EE3\u66FF</p><p>\u63D0\u793A\u5185\u5BB9</p></div><div class="info custom-block"><p class="custom-block-title">INFO</p><p>INFO\u6D88\u606F</p></div><div class="warning custom-block"><p class="custom-block-title">WARNING</p><p>WARNING\u6D88\u606F <a>a\u94FE\u63A5</a></p></div><div class="danger custom-block"><p class="custom-block-title">DANGER</p><p>DANGER\u6D88\u606F <a href="./example.html">md\u94FE\u63A5</a></p></div><details class="details custom-block"><summary>Details</summary><p>\u8BE6\u7EC6\u4FE1\u606F</p></details>`,8);function d(n,b,u,A,C,_){return e(),l("div",null,[s("h1",i,[p(c(n.$frontmatter.title)+" ",1),o]),m])}const y=a(r,[["render",d]]);export{D as __pageData,y as default};
